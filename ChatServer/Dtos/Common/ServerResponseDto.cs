272ee2536df3c51ebe4751acd69ae82ff93eff15
dSandley20 <danielsandley2@gmail.com>
2022-08-08 13:59:15 -0400

    generic server response model

---
+++ ChatServer/Dtos/Common/ServerResponseDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace ChatServer.Dtos.Common
+{
+    public class ServerResponseDto
+    {
+        public string message { get; set; }
+        public object data { get; set; }
+    }
+}
