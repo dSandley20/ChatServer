0605219e644d41fdf223c2d48d2972e71f23c8a3
dSandley20 <danielsandley2@gmail.com>
2022-08-08 13:59:03 -0400

    consolidate Auth DTOs

---
+++ ChatServer/Dtos/Auth/AuthDto.cs
@@ -0,0 +1,27 @@
+﻿using ChatServer.Dtos.User;
+
+namespace ChatServer.Dtos.Auth
+{
+    public class AuthDto
+    {
+        public string token { get; set; }
+
+        public ClientUserDto user { get; set; }
+    }
+
+    public class UserLoginDto
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+
+    public class CreateUserDto
+    {
+        public string Email { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+    }
+}
