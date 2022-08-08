535fa004c7b3f3fe5082a2a4f47be17a19c98e1c
dSandley20 <danielsandley2@gmail.com>
2022-08-08 14:42:29 -0400

    use exception handling middleware

--- ChatServer/Startup.cs
+++ ChatServer/Startup.cs
@@ -1,6 +1,9 @@
 ﻿using System.Reflection;
 using System.Text;
+using ChatServer.Hubs;
 using ChatServer.Migrations;
+using ChatServer.Repositories;
+using ChatServer.RepositoryInterfaces;
 using ChatServer.Utilities;
 using FluentMigrator.Runner;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -10,15 +13,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
-using ChatServer.Repositories;
-using ChatServer.RepositoryInterfaces;
-using ChatServer.Hubs;
 
 namespace ChatServer
 {
     public class Startup
     {
-
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -29,20 +28,24 @@ namespace ChatServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                    .AddJwtBearer(options =>
-                    {
-                        options.TokenValidationParameters = new TokenValidationParameters
-                        {
+            services
+                .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters =
+                        new TokenValidationParameters {
                             ValidateIssuer = true,
                             ValidateAudience = true,
                             ValidateLifetime = true,
                             ValidateIssuerSigningKey = true,
                             ValidIssuer = Configuration["Jwt:Issuer"],
                             ValidAudience = Configuration["Jwt:Audiance"],
-                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                            IssuerSigningKey =
+                                new SymmetricSecurityKey(Encoding
+                                        .UTF8
+                                        .GetBytes(Configuration["Jwt:Key"]))
                         };
-                    });
+                });
 
             services.AddSingleton<IAuthRepository, AuthRepository>();
             services.AddSingleton<IUserRepository, UserRepository>();
@@ -52,58 +55,57 @@ namespace ChatServer
 
             services.AddControllers();
 
-            services.AddCors(options =>
-            {
-                options.AddDefaultPolicy(
-                    policy =>
-                    {
-                        policy.WithOrigins("http://localhost:3000").AllowAnyHeader().AllowAnyMethod();
-                    });
-            });
+            services
+                .AddCors(options =>
+                {
+                    options
+                        .AddDefaultPolicy(policy =>
+                        {
+                            policy
+                                .WithOrigins("http://localhost:3000")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                        });
+                });
 
             services
                 .AddLogging(c => c.AddFluentMigratorConsole())
                 .AddFluentMigratorCore()
-                .ConfigureRunner(c => c
-                .AddPostgres()
-                .WithGlobalConnectionString(Configuration["DB:Connection"])
-                .ScanIn(Assembly.GetExecutingAssembly()).For.All());
+                .ConfigureRunner(c =>
+                    c
+                        .AddPostgres()
+                        .WithGlobalConnectionString(Configuration["DB:Connection"])
+                        .ScanIn(Assembly.GetExecutingAssembly())
+                        .For
+                        .All());
 
             services.AddSignalR();
         }
 
-
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseExceptionHandler("/error");
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
             app.UseCors();
-
             app.UseAuthentication();
-
             app.UseAuthorization();
-            
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-                endpoints.MapHub<ChatHub>("/chat");
-                //Add WS Hub
-            });
+            app
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                    endpoints.MapHub<ChatHub>("/chat");
+                    //Add WS Hub
+                });
 
-            Database.EnsureDatabase(Configuration["DB:Connection"],
-       "chat_clone");
+            Database
+                .EnsureDatabase(Configuration["DB:Connection"], "chat_clone");
             app.Migrate();
         }
-
-
     }
 }
