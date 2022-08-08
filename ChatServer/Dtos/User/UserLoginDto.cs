76e8be90cfa6df05c979110ce86e0c2d7389af20
dSandley20 <danielsandley2@gmail.com>
2022-08-08 13:59:42 -0400

    remove unnceccessary files

--- ChatServer/Dtos/User/UserLoginDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace ChatServer.Dtos.Users
-{
-    public class UserLoginDto
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
