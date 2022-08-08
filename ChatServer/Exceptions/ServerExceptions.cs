687937bdf8ce1174783563000cb4643b01e2d852
dSandley20 <danielsandley2@gmail.com>
2022-08-08 14:01:39 -0400

    custom exception messages for servers and users

---
+++ ChatServer/Exceptions/ServerExceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ChatServer.Exceptions
+{
+    [Serializable]
+    public class ServerNotFound : Exception
+    {
+        public ServerNotFound(string message) : base(message){}
+    }
+
+    [Serializable]
+    public class ServerCreationError : Exception
+    {
+        public ServerCreationError(string message) : base(message) { }
+    }
+
+}
---
+++ ChatServer/Exceptions/UserExceptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ChatServer.Exceptions
+{
+    [Serializable]
+    public class UserNotFound : Exception
+    {
+        public UserNotFound(string message) : base(message) { }
+    }
+
+    [Serializable]
+    public class UserUpdateError : Exception
+    {
+        public UserUpdateError(string message): base(message) { }
+    }
+
+    [Serializable]
+    public class UserCreateError : Exception
+    {
+        public UserCreateError(string message): base(message) { }
+    }
+}
+
